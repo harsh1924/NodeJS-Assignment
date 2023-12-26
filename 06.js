ea1b1f7915efde2ae11a3cec3c02baa818439e66
harsh1924 <harshrpanwar@gmail.com>
2023-12-26 18:29:04 +0530

    Sixth Assignment

---
+++ 06.js
@@ -0,0 +1,6 @@
+const os = require('os');
+console.log('Operating System Name: ' + os.type());
+console.log('OS Release: ' + os.release());
+
+// OUTPUT: Operating System Name: Windows_NT
+//         OS Release: 10.0.22631
